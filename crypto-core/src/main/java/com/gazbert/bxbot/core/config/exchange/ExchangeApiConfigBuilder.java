600d5d6efdb1b0e63ed61fcf84e652fcd6779829
0xlotus <89240464+0xlotus@users.noreply.github.com>
2021-03-21 13:50:02 +0000

    NEWS.

--- crypto-core/src/main/java/com/gazbert/bxbot/core/config/exchange/ExchangeApiConfigBuilder.java
+++ crypto-core/src/main/java/com/gazbert/bxbot/core/config/exchange/ExchangeApiConfigBuilder.java
@@ -15,4 +15,39 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.core.config.exchange;
+
+import com.gazbert.crypto.domain.exchange.ExchangeConfig;
+import com.gazbert.crypto.domain.exchange.NetworkConfig;
+import java.util.List;
+import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Util class for building the Exchange API config.
+ *
+ * @author gazbert
+ */
+public final class ExchangeApiConfigBuilder {
+
+  private static final Logger LOG = LogManager.getLogger();
+
+  private ExchangeApiConfigBuilder() {
+  }
+
+  /** Builds Exchange API config. */
+  public static ExchangeConfigImpl buildConfig(ExchangeConfig exchangeConfig) {
+
+    final ExchangeConfigImpl exchangeApiConfig = new ExchangeConfigImpl();
+    exchangeApiConfig.setExchangeName(exchangeConfig.getName());
+    exchangeApiConfig.setExchangeAdapter(exchangeConfig.getAdapter());
+
+    final NetworkConfig networkConfig = exchangeConfig.getNetworkConfig();
+    if (ne
