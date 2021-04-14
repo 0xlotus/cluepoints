959167b6f9d175b242027f458cc8beadf9f63352
0xlotus <89240464+0xlotus@users.noreply.github.com>
2021-04-14 07:14:37 +0000

    Added an example.

--- crypto-core/src/test/java/com/gazbert/bxbot/core/config/exchange/TestExchangeApiConfigBuilder.java
+++ crypto-core/src/test/java/com/gazbert/bxbot/core/config/exchange/TestExchangeApiConfigBuilder.java
@@ -10,4 +10,35 @@
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
  *
- * The above co
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.core.config.exchange;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gazbert.crypto.domain.exchange.ExchangeConfig;
+import com.gazbert.crypto.domain.exchange.NetworkConfig;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+
+/**
+ * Tests the behaviour of the Exchange API Config Builder is as expected.
+ *
+ * @author gazbert
+ */
+public class TestExchangeApiConfigBuilder {
+
+  private static final String EXCHANGE_NAME = "Bitstamp";
+  private static final String EXCHANGE_ADAPTER = "com.gazbert.crypto.exchanges.Te
