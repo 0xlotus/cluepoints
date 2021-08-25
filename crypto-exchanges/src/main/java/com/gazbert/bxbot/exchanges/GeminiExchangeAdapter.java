d218b8ee9f9015d30c35974f14de45e3c6ff974b
0xlotus <89240464+0xlotus@users.noreply.github.com>
2021-08-25 21:27:44 +0000

    create github pages

--- crypto-exchanges/src/main/java/com/gazbert/bxbot/exchanges/GeminiExchangeAdapter.java
+++ crypto-exchanges/src/main/java/com/gazbert/bxbot/exchanges/GeminiExchangeAdapter.java
@@ -361,3 +361,478 @@ public final class GeminiExchangeAdapter extends AbstractExchangeAdapter
       final List<MarketOrder> buyOrders = new ArrayList<>();
       for (GeminiMarketOrder geminiBuyOrder : orderBook.bids) {
         final MarketOrder buyOrder =
+            new MarketOrderImpl(
+                OrderType.BUY,
+                geminiBuyOrder.price,
+                geminiBuyOrder.amount,
+                geminiBuyOrder.price.multiply(geminiBuyOrder.amount));
+        buyOrders.add(buyOrder);
+      }
+
+      final List<MarketOrder> sellOrders = new ArrayList<>();
+      for (GeminiMarketOrder geminiSellOrder : orderBook.asks) {
+        final MarketOrder sellOrder =
+            new MarketOrderImpl(
+                OrderType.SELL,
+                geminiSellOrder.price,
+                geminiSellOrder.amount,
+                geminiSellOrder.price.multiply(geminiSellOrder.amount));
+        sellOrders.add(sellOrder);
+      }
+
+      return new MarketOrderBookImpl(marketId, sellOrders, buyOrders);
+
+    } catch (ExchangeNetworkException | TradingApiException e) {
+      throw e;
+
+    } catch (Exception e) {
+      LOG.error(UNEXPECTED_ERROR_MSG, e);
+      throw new TradingApiException(UNEXPECTED_ERROR_MSG, e);
+    }
+  }
+
+  @Override
+  public BigDecimal getLatestMarketPrice(String marketId)
+      throws TradingApiException, ExchangeNetworkException {
+    try {
+      final ExchangeHttpResponse response = sendPublicRequestToExchange("pubticker/" + marketId);
+
+      LOG.debug(() -> "Latest Market Price response: " + response);
+
+      final GeminiTicker ticker = gson.fromJson(response.getPayload(), GeminiTicker.class);
+      return ticker.last;
+
+    } catch (ExchangeNetworkException | TradingApiException e) {
+      throw e;
+
+    } catch (Exception e) {
+      LOG.error(UNEXPECTED_ERROR_MSG, e);
+      throw new TradingApiException(UNEXPECTED_ERROR_MSG, e);
+    }
+  }
+
+  @Override
+  public BalanceInfo getBalanceInfo() throws TradingApiException, ExchangeNetworkException {
+    try {
+      final ExchangeHttpResponse response = sendAuthenticatedRequestToExchange("balances", null);
+
+      LOG.debug(() -> "Balance Info response: " + response);
+
+      final GeminiBalances allAccountBalances =
+          gson.fromJson(response.getPayload(), GeminiBalances.class);
+      final HashMap<String, BigDecimal> balancesAvailable = new HashMap<>();
+
+      // This adapter only supports 'exchange' account type.
+      allAccountBalances.stream()
+          .filter(accountBalance -> accountBalance.type.equalsIgnoreCase("exchange"))
+          .forEach(
+              accountBalance ->
+                  balancesAvailable.put(accountBalance.currency, accountBalance.available));
+
+      // 2nd arg of BalanceInfo constructor for reserved/on-hold balances is not provided by
+      // exchange.
+      return new BalanceInfoImpl(balancesAvailable, new HashMap<>());
+
+    } catch (ExchangeNetworkException | TradingApiException e) {
+      throw e;
+
+    } catch (Exception e) {
+      LOG.error(UNEXPECTED_ERROR_MSG, e);
+      throw new TradingApiException(UNEXPECTED_ERROR_MSG, e);
+    }
+  }
+
+  /*
+   * Gemini does not provide API call for fetching % buy fee. We load the % fee statically
+   * from exchange.yaml file - see https://gemini.com/fee-schedule/
+   */
+  @Override
+  public BigDecimal getPercentageOfBuyOrderTakenForExchangeFee(String marketId) {
+    return buyFeePercentage;
+  }
+
+  /*
+   * Gemini does not provide API call for fetching % sell fee. load the % fee statically
+   * from exchange.yaml file - see https://gemini.com/fee-schedule/
+   */
+  @Override
+  public BigDecimal getPercentageOfSellOrderTakenForExchangeFee(String marketId) {
+    return sellFeePercentage;
+  }
+
+  @Override
+  public String getImplName() {
+    return "Gemini REST API v1";
+  }
+
+  // --------------------------------------------------------------------------
+  //  GSON classes for JSON responses.
+  //  See https://docs.gemini.com/rest-api/
+  // --------------------------------------------------------------------------
+
+  /** GSON class for a market Order Book. */
+  private static class GeminiOrderBook {
+
+    List<GeminiMarketOrder> bids;
+    List<GeminiMarketOrder> asks;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this).add("bids", bids).add("asks", asks).toString();
+    }
+  }
+
+  /** GSON class for a Market Order. */
+  private static class GeminiMarketOrder {
+
+    BigDecimal price;
+    BigDecimal amount;
+    // ignore the timestamp attribute as per the API spec
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this).add(PRICE, price).add(AMOUNT, amount).toString();
+    }
+  }
+
+  /** GSON class for Balances API call response. */
+  private static class GeminiBalances extends ArrayList<GeminiAccountBalance> {
+
+    private static final long serialVersionUID = 5516523141993401253L;
+  }
+
+  /**
+   * GSON class for holding account type balance info. This adapter only supports type 'exchange'.
+   */
+  private static class GeminiAccountBalance {
+
+    String type;
+    String currency;
+    BigDecimal amount;
+    BigDecimal available;
+    BigDecimal availableForWithdrawal;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("type", type)
+          .add("currency", currency)
+          .add(AMOUNT, amount)
+          .add("available", available)
+          .add("availableForWithdrawal", availableForWithdrawal)
+          .toString();
+    }
+  }
+
+  /** GSON class for Ticker API call response. */
+  private static class GeminiTicker {
+
+    BigDecimal bid;
+    BigDecimal ask;
+    BigDecimal last;
+    BigDecimal low;
+    GeminiVolume volume;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("bid", bid)
+          .add("ask", ask)
+          .add("last", last)
+          .add("low", low)
+          .add("volume", volume)
+          .toString();
+    }
+  }
+
+  /** GSON class for holding volume information in the Ticker response. */
+  private static class GeminiVolume {
+
+    @SerializedName("BTC")
+    BigDecimal btc;
+
+    @SerializedName("USD")
+    BigDecimal usd;
+
+    long timestamp;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("btc", btc)
+          .add("usd", usd)
+          .add("timestamp", timestamp)
+          .toString();
+    }
+  }
+
+  /** GSON class for holding an active orders API call response. */
+  private static class GeminiOpenOrders extends ArrayList<GeminiOpenOrder> {
+
+    private static final long serialVersionUID = 5516523611153405953L;
+  }
+
+  /** GSON class representing an open order on the exchange. */
+  private static class GeminiOpenOrder {
+
+    @SerializedName("order_id")
+    long orderId; // use this value for order id as per the API spec
+
+    long id;
+    String symbol;
+    String exchange;
+    BigDecimal price;
+
+    @SerializedName("avg_execution_price")
+    BigDecimal avgExecutionPrice;
+
+    String side; // buy|sell
+    String type; // exchange limit
+    String timestamp; // timestamp as a String
+    long timestampms; // timestamp in millis as a long
+
+    @SerializedName("is_live")
+    boolean isLive;
+
+    @SerializedName("is_cancelled")
+    boolean isCancelled;
+
+    @SerializedName("is_hidden")
+    boolean isHidden;
+
+    @SerializedName("was_forced")
+    boolean wasForced;
+
+    @SerializedName("remaining_amount")
+    BigDecimal remainingAmount;
+
+    @SerializedName("executed_amount")
+    BigDecimal executedAmount;
+
+    @SerializedName("original_amount")
+    BigDecimal originalAmount;
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("orderId", orderId)
+          .add("id", id)
+          .add("symbol", symbol)
+          .add("exchange", exchange)
+          .add(PRICE, price)
+          .add("avgExecutionPrice", avgExecutionPrice)
+          .add("side", side)
+          .add("type", type)
+          .add("timestamp", timestamp)
+          .add("timestampms", timestampms)
+          .add("isLive", isLive)
+          .add("isCancelled", isCancelled)
+          .add("isHidden", isHidden)
+          .add("wasForced", wasForced)
+          .add("remainingAmount", remainingAmount)
+          .add("executedAmount", executedAmount)
+          .add("originalAmount", originalAmount)
+          .toString();
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  //  Transport layer
+  // --------------------------------------------------------------------------
+
+  private ExchangeHttpResponse sendPublicRequestToExchange(String apiMethod)
+      throws ExchangeNetworkException, TradingApiException {
+    try {
+      final URL url = new URL(PUBLIC_API_BASE_URL + apiMethod);
+      return makeNetworkRequest(url, "GET", null, createRequestParamMap());
+
+    } catch (MalformedURLException e) {
+      final String errorMsg = UNEXPECTED_IO_ERROR_MSG;
+      LOG.error(errorMsg, e);
+      throw new TradingApiException(errorMsg, e);
+    }
+  }
+
+  /*
+   * Makes an authenticated API call to the Gemini exchange.
+   *
+   * E.g.
+   *
+   * POST https://api.gemini.com/v0/order/new
+   *
+   * With JSON payload of:
+   * {
+   *    // Standard headers
+   *    "request": "/v1/order/new",
+   *    "nonce": <nonce>,
+   *
+   *    // Request-specific data
+   *    "order_id": 18834
+   * }
+   *
+   * To authenticate a request, we must calculate the following:
+   *
+   * b64 = base64.b64encode("""{
+   * "request": "/v1/order/status",
+   * "nonce": 123456,
+   * "order_id": 18834
+   * }
+   * """)
+   *
+   * Whitespace is ignored by the server, and may be included if desired.
+   * The hashes are always taken on the base64 string directly, with no normalization,
+   * so whatever is sent in the payload is what should be hashed, and what the server will verify.
+   *
+   * Finally, the HMac:
+   *
+   * hmac.new("privateKey", b64, hashlib.sha384).hexdigest()
+   * '337cc8b4ea692cfe65b4a85fcc9f042b2e3f702ac956fd098d600ab15705775017beae402be773ceee107170d710f'
+   *
+   * These are sent as HTTP headers:
+   *
+   * X-GEMINI-APIKEY: apiKey
+   * X-GEMINI-PAYLOAD:ewogICAgInJlcXVlc3QiOiAiL3YxL29yZGVyL3N
+   * X-GEMINI-SIGNATURE: 337cc8b4ea692cfe65b4a85fcc9f042b2e3f
+   */
+  private ExchangeHttpResponse sendAuthenticatedRequestToExchange(
+      String apiMethod, Map<String, String> params)
+      throws ExchangeNetworkException, TradingApiException {
+
+    if (!initializedMacAuthentication) {
+      final String errorMsg = "MAC Message security layer has not been initialized.";
+      LOG.error(errorMsg);
+      throw new IllegalStateException(errorMsg);
+    }
+
+    try {
+      if (params == null) {
+        // create empty map for non param API calls, e.g. "balances"
+        params = createRequestParamMap();
+      }
+
+      // Add the API call method
+      params.put("request", "/" + GEMINI_API_VERSION + "/" + apiMethod);
+
+      // nonce is required by Gemini in every request
+      params.put("nonce", Long.toString(nonce));
+      nonce++;
+
+      // JSON-ify the param dictionary
+      final String paramsInJson = gson.toJson(params);
+
+      // Need to base64 encode payload as per API
+      final String base64payload =
+          DatatypeConverter.printBase64Binary(paramsInJson.getBytes(StandardCharsets.UTF_8));
+
+      // Create the signature
+      mac.reset(); // force reset
+      mac.update(base64payload.getBytes(StandardCharsets.UTF_8));
+      final String signature = toHex(mac.doFinal()).toLowerCase();
+
+      // Request headers required by Exchange
+      final Map<String, String> requestHeaders = createHeaderParamMap();
+      requestHeaders.put("X-GEMINI-APIKEY", key);
+      requestHeaders.put("X-GEMINI-PAYLOAD", base64payload);
+      requestHeaders.put("X-GEMINI-SIGNATURE", signature);
+
+      // payload is JSON for this exchange
+      requestHeaders.put("Content-Type", "application/json");
+
+      final URL url = new URL(AUTHENTICATED_API_URL + apiMethod);
+      return makeNetworkRequest(url, "POST", paramsInJson, requestHeaders);
+
+    } catch (MalformedURLException e) {
+      final String errorMsg = UNEXPECTED_IO_ERROR_MSG;
+      LOG.error(errorMsg, e);
+      throw new TradingApiException(errorMsg, e);
+    }
+  }
+
+  private String toHex(byte[] byteArrayToConvert) {
+    final StringBuilder hexString = new StringBuilder();
+    for (final byte aByte : byteArrayToConvert) {
+      hexString.append(String.format("%02x", aByte & 0xff));
+    }
+    return hexString.toString();
+  }
+
+  /*
+   * Initialises the secure messaging layer.
+   * Sets up the MAC to safeguard the data we send to the exchange.
+   * Used to encrypt the hash of the entire message with the private key to ensure message
+   * integrity. We fail hard n fast if any of this stuff blows.
+   */
+  private void initSecureMessageLayer() {
+    try {
+      final SecretKeySpec keyspec =
+          new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA384");
+      mac = Mac.getInstance("HmacSHA384");
+      mac.init(keyspec);
+      initializedMacAuthentication = true;
+    } catch (NoSuchAlgorithmException e) {
+      final String errorMsg = "Failed to setup MAC security. HINT: Is HMAC-SHA384 installed?";
+      LOG.error(errorMsg, e);
+      throw new IllegalStateException(errorMsg, e);
+    } catch (InvalidKeyException e) {
+      final String errorMsg = "Failed to setup MAC security. Secret key seems invalid!";
+      LOG.error(errorMsg, e);
+      throw new IllegalArgumentException(errorMsg, e);
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  //  Config methods
+  // --------------------------------------------------------------------------
+
+  private void setAuthenticationConfig(ExchangeConfig exchangeConfig) {
+    final AuthenticationConfig authenticationConfig = getAuthenticationConfig(exchangeConfig);
+    key = getAuthenticationConfigItem(authenticationConfig, KEY_PROPERTY_NAME);
+    secret = getAuthenticationConfigItem(authenticationConfig, SECRET_PROPERTY_NAME);
+  }
+
+  private void setOtherConfig(ExchangeConfig exchangeConfig) {
+    final OtherConfig otherConfig = getOtherConfig(exchangeConfig);
+
+    final String buyFeeInConfig = getOtherConfigItem(otherConfig, BUY_FEE_PROPERTY_NAME);
+    buyFeePercentage =
+        new BigDecimal(buyFeeInConfig).divide(new BigDecimal("100"), 8, RoundingMode.HALF_UP);
+    LOG.info(() -> "Buy fee % in BigDecimal format: " + buyFeePercentage);
+
+    final String sellFeeInConfig = getOtherConfigItem(otherConfig, SELL_FEE_PROPERTY_NAME);
+    sellFeePercentage =
+        new BigDecimal(sellFeeInConfig).divide(new BigDecimal("100"), 8, RoundingMode.HALF_UP);
+    LOG.info(() -> "Sell fee % in BigDecimal format: " + sellFeePercentage);
+  }
+
+  // --------------------------------------------------------------------------
+  //  Util methods
+  // --------------------------------------------------------------------------
+
+  private void initGson() {
+    final GsonBuilder gsonBuilder = new GsonBuilder();
+    gson = gsonBuilder.create();
+  }
+
+  /*
+   * Hack for unit-testing map params passed to transport layer.
+   */
+  private Map<String, String> createRequestParamMap() {
+    return new HashMap<>();
+  }
+
+  /*
+   * Hack for unit-testing header params passed to transport layer.
+   */
+  private Map<String, String> createHeaderParamMap() {
+    return new HashMap<>();
+  }
+
+  /*
+   * Hack for unit-testing transport layer.
+   */
+  private ExchangeHttpResponse makeNetworkRequest(
+      URL url, String httpMethod, String postData, Map<String, String> requestHeaders)
+      throws TradingApiException, ExchangeNetworkException {
+    return super.sendNetworkRequest(url, httpMethod, postData, requestHeaders);
+  }
+}
