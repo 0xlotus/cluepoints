db34e73e3273b585f79855f0cdab376e8a3b8712
0xlotus <89240464+0xlotus@users.noreply.github.com>
2021-08-28 10:43:11 +0000

    ore: Remove support of partial IO request (NFS crash)

--- crypto-exchanges/src/main/java/com/gazbert/bxbot/exchanges/KrakenExchangeAdapter.java
+++ crypto-exchanges/src/main/java/com/gazbert/bxbot/exchanges/KrakenExchangeAdapter.java
@@ -800,3 +800,438 @@ public final class KrakenExchangeAdapter extends AbstractExchangeAdapter
         // assume 1 (KV) entry as per API spec - the K is the market id, the V is a Map of ticker
         // params
         final JsonElement tickerParams = jsonObject.entrySet().iterator().next().getValue();
+
+        final JsonObject tickerMap = tickerParams.getAsJsonObject();
+        for (Map.Entry<String, JsonElement> jsonTickerParam : tickerMap.entrySet()) {
+
+          final String key = jsonTickerParam.getKey();
+          switch (key) {
+            case "c":
+              final List<String> lastTradeDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("c", lastTradeDetails.get(0));
+              break;
+
+            case "b":
+              final List<String> bidDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("b", bidDetails.get(0));
+              break;
+
+            case "a":
+              final List<String> askDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("a", askDetails.get(0));
+              break;
+
+            case "l":
+              final List<String> lowDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("l", lowDetails.get(1));
+              break;
+
+            case "h":
+              final List<String> highDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("h", highDetails.get(1));
+              break;
+
+            case "o":
+              final String openDetails =
+                  context.deserialize(jsonTickerParam.getValue(), String.class);
+              krakenTickerResult.put("o", openDetails);
+              break;
+
+            case "v":
+              final List<String> volumeDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("v", volumeDetails.get(1));
+              break;
+
+            case "p":
+              final List<String> vWapDetails =
+                  context.deserialize(jsonTickerParam.getValue(), List.class);
+              krakenTickerResult.put("p", vWapDetails.get(1));
+              break;
+
+            default:
+              LOG.warn(() -> "Received unexpected Ticker param - ignoring: " + key);
+          }
+        }
+      }
+      return krakenTickerResult;
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  //  Transport layer methods
+  // --------------------------------------------------------------------------
+
+  private ExchangeHttpResponse sendPublicRequestToExchange(
+      String apiMethod, Map<String, String> params)
+      throws ExchangeNetworkException, TradingApiException {
+
+    if (params == null) {
+      params = createRequestParamMap(); // no params, so empty query string
+    }
+
+    // Request headers required by Exchange
+    final Map<String, String> requestHeaders = createHeaderParamMap();
+
+    try {
+      final StringBuilder queryString = new StringBuilder();
+      if (!params.isEmpty()) {
+        queryString.append("?");
+        for (final Map.Entry<String, String> param : params.entrySet()) {
+          if (queryString.length() > 1) {
+            queryString.append("&");
+          }
+          queryString.append(param.getKey());
+          queryString.append("=");
+          queryString.append(URLEncoder.encode(param.getValue(), StandardCharsets.UTF_8));
+        }
+
+        requestHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+      }
+
+      final URL url = new URL(PUBLIC_API_BASE_URL + apiMethod + queryString);
+      return makeNetworkRequest(url, "GET", null, requestHeaders);
+
+    } catch (MalformedURLException e) {
+      final String errorMsg = UNEXPECTED_IO_ERROR_MSG;
+      LOG.error(errorMsg, e);
+      throw new TradingApiException(errorMsg, e);
+    }
+  }
+
+  /*
+   * Makes an authenticated API call to the Kraken exchange.
+   *
+   * Kraken requires the following HTTP headers to bet set:
+   *
+   * API-Key = API key
+   * API-Sign = Message signature using HMAC-SHA512 of (URI path + SHA256(nonce + POST data))
+   *            and base64 decoded secret API key
+   *
+   * The nonce must always increasing unsigned 64 bit integer.
+   *
+   * Note: Sometimes requests can arrive out of order or NTP can cause your clock to rewind,
+   * resulting in nonce issues. If you encounter this issue, you can change the nonce window in
+   * your account API settings page. The amount to set it to depends upon how you increment the
+   * nonce. Depending on your connectivity, a setting that would accommodate 3-15 seconds of
+   * network issues is suggested.
+   */
+  private ExchangeHttpResponse sendAuthenticatedRequestToExchange(
+      String apiMethod, Map<String, String> params)
+      throws ExchangeNetworkException, TradingApiException {
+
+    if (!initializedMacAuthentication) {
+      final String errorMsg = "MAC Message security layer has not been initialized.";
+      LOG.error(errorMsg);
+      throw new IllegalStateException(errorMsg);
+    }
+
+    try {
+      if (params == null) {
+        // create empty map for non param API calls, e.g. "trades"
+        params = createRequestParamMap();
+      }
+
+      // The nonce is required by Kraken in every request.
+      // It MUST be incremented each time and the nonce param MUST match the value used in
+      // signature.
+      nonce++;
+      params.put("nonce", Long.toString(nonce));
+
+      // Build the URL with query param args in it - yuk!
+      final StringBuilder postData = new StringBuilder();
+      for (final Map.Entry<String, String> param : params.entrySet()) {
+        if (postData.length() > 0) {
+          postData.append("&");
+        }
+        postData.append(param.getKey());
+        postData.append("=");
+        postData.append(URLEncoder.encode(param.getValue(), StandardCharsets.UTF_8));
+      }
+
+      // And now the tricky part... ;-o
+      final byte[] pathInBytes =
+          ("/" + KRAKEN_API_VERSION + KRAKEN_PRIVATE_PATH + apiMethod)
+              .getBytes(StandardCharsets.UTF_8);
+      final String noncePrependedToPostData = Long.toString(nonce) + postData;
+
+      // Create sha256 hash of nonce and post data:
+      final MessageDigest md = MessageDigest.getInstance("SHA-256");
+      md.update(noncePrependedToPostData.getBytes(StandardCharsets.UTF_8));
+      final byte[] messageHash = md.digest();
+
+      // Create hmac_sha512 digest of path and previous sha256 hash
+      mac.reset(); // force reset
+      mac.update(pathInBytes);
+      mac.update(messageHash);
+
+      // Signature in Base64
+      final String signature = Base64.getEncoder().encodeToString(mac.doFinal());
+
+      // Request headers required by Exchange
+      final Map<String, String> requestHeaders = createHeaderParamMap();
+      requestHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+      requestHeaders.put("API-Key", key);
+      requestHeaders.put("API-Sign", signature);
+
+      final URL url = new URL(AUTHENTICATED_API_URL + apiMethod);
+      return makeNetworkRequest(url, "POST", postData.toString(), requestHeaders);
+
+    } catch (MalformedURLException | NoSuchAlgorithmException e) {
+      final String errorMsg = UNEXPECTED_IO_ERROR_MSG;
+      LOG.error(errorMsg, e);
+      throw new TradingApiException(errorMsg, e);
+    }
+  }
+
+  /*
+   * Initialises the secure messaging layer.
+   * Sets up the MAC to safeguard the data we send to the exchange.
+   * Used to encrypt the hash of the entire message with the private key to ensure message
+   * integrity. We fail hard n fast if any of this stuff blows.
+   */
+  private void initSecureMessageLayer() {
+    try {
+      // Kraken secret key is in Base64, so we need to decode it first
+      final byte[] base64DecodedSecret = Base64.getDecoder().decode(secret);
+
+      final SecretKeySpec keyspec = new SecretKeySpec(base64DecodedSecret, "HmacSHA512");
+      mac = Mac.getInstance("HmacSHA512");
+      mac.init(keyspec);
+      initializedMacAuthentication = true;
+    } catch (NoSuchAlgorithmException e) {
+      final String errorMsg = "Failed to setup MAC security. HINT: Is HmacSHA512 installed?";
+      LOG.error(errorMsg, e);
+      throw new IllegalStateException(errorMsg, e);
+    } catch (InvalidKeyException e) {
+      final String errorMsg = "Failed to setup MAC security. Secret key seems invalid!";
+      LOG.error(errorMsg, e);
+      throw new IllegalArgumentException(errorMsg, e);
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  //  Config methods
+  // --------------------------------------------------------------------------
+
+  private void setAuthenticationConfig(ExchangeConfig exchangeConfig) {
+    final AuthenticationConfig authenticationConfig = getAuthenticationConfig(exchangeConfig);
+    key = getAuthenticationConfigItem(authenticationConfig, KEY_PROPERTY_NAME);
+    secret = getAuthenticationConfigItem(authenticationConfig, SECRET_PROPERTY_NAME);
+  }
+
+  private void setOtherConfig(ExchangeConfig exchangeConfig) {
+    final OtherConfig otherConfig = getOtherConfig(exchangeConfig);
+
+    final String buyFeeInConfig = getOtherConfigItem(otherConfig, BUY_FEE_PROPERTY_NAME);
+    buyFeePercentage =
+        new BigDecimal(buyFeeInConfig).divide(new BigDecimal("100"), 8, RoundingMode.HALF_UP);
+    LOG.info(() -> "Buy fee % in BigDecimal format: " + buyFeePercentage);
+
+    final String sellFeeInConfig = getOtherConfigItem(otherConfig, SELL_FEE_PROPERTY_NAME);
+    sellFeePercentage =
+        new BigDecimal(sellFeeInConfig).divide(new BigDecimal("100"), 8, RoundingMode.HALF_UP);
+    LOG.info(() -> "Sell fee % in BigDecimal format: " + sellFeePercentage);
+
+    final String keepAliveDuringMaintenanceConfig =
+        getOtherConfigItem(otherConfig, KEEP_ALIVE_DURING_MAINTENANCE_PROPERTY_NAME);
+    if (!keepAliveDuringMaintenanceConfig.isEmpty()) {
+      keepAliveDuringMaintenance = Boolean.valueOf(keepAliveDuringMaintenanceConfig);
+      LOG.info(() -> "Keep Alive During Maintenance: " + keepAliveDuringMaintenance);
+    } else {
+      LOG.info(() -> KEEP_ALIVE_DURING_MAINTENANCE_PROPERTY_NAME + " is not set in exchange.yaml");
+    }
+  }
+
+  // --------------------------------------------------------------------------
+  //  Util methods
+  // --------------------------------------------------------------------------
+
+  private List<OpenOrder> adaptKrakenOpenOrders(KrakenResponse krakenResponse, String marketId)
+      throws TradingApiException {
+    final List<OpenOrder> openOrders = new ArrayList<>();
+
+    // Assume we'll always get something here if errors array is empty; else blow fast wih NPE
+    final KrakenOpenOrderResult krakenOpenOrderResult =
+        (KrakenOpenOrderResult) krakenResponse.result;
+
+    final Map<String, KrakenOpenOrder> krakenOpenOrders = krakenOpenOrderResult.open;
+    if (krakenOpenOrders != null) {
+      for (final Map.Entry<String, KrakenOpenOrder> openOrder : krakenOpenOrders.entrySet()) {
+
+        OrderType orderType;
+        final KrakenOpenOrder krakenOpenOrder = openOrder.getValue();
+        final KrakenOpenOrderDescription krakenOpenOrderDescription = krakenOpenOrder.descr;
+
+        if (!marketId.equalsIgnoreCase(krakenOpenOrderDescription.pair)) {
+          continue;
+        }
+
+        switch (krakenOpenOrderDescription.type) {
+          case "buy":
+            orderType = OrderType.BUY;
+            break;
+          case "sell":
+            orderType = OrderType.SELL;
+            break;
+          default:
+            throw new TradingApiException(
+                "Unrecognised order type received in getYourOpenOrders(). Value: "
+                    + openOrder.getValue().descr.ordertype);
+        }
+
+        final OpenOrder order =
+            new OpenOrderImpl(
+                openOrder.getKey(),
+                new Date((long) krakenOpenOrder.opentm), // opentm == creationDate
+                marketId,
+                orderType,
+                krakenOpenOrderDescription.price,
+                // vol_exec == amount of order that has been executed
+                (krakenOpenOrder.vol.subtract(krakenOpenOrder.volExec)),
+                krakenOpenOrder.vol, // vol == orig order amount
+                // krakenOpenOrder.cost, // cost == total value of order in API docs, but it's
+                // always 0 :-(
+                krakenOpenOrderDescription.price.multiply(krakenOpenOrder.vol));
+
+        openOrders.add(order);
+      }
+    }
+    return openOrders;
+  }
+
+  private MarketOrderBookImpl adaptKrakenOrderBook(KrakenResponse krakenResponse, String marketId)
+      throws TradingApiException {
+
+    // Assume we'll always get something here if errors array is empty; else blow fast wih NPE
+    final KrakenMarketOrderBookResult krakenOrderBookResult =
+        (KrakenMarketOrderBookResult) krakenResponse.result;
+    final Optional<KrakenOrderBook> first = krakenOrderBookResult.values().stream().findFirst();
+    if (first.isPresent()) {
+      final KrakenOrderBook krakenOrderBook = first.get();
+
+      final List<MarketOrder> buyOrders = new ArrayList<>();
+      for (KrakenMarketOrder krakenBuyOrder : krakenOrderBook.bids) {
+        final MarketOrder buyOrder =
+            new MarketOrderImpl(
+                OrderType.BUY,
+                krakenBuyOrder.get(0),
+                krakenBuyOrder.get(1),
+                krakenBuyOrder.get(0).multiply(krakenBuyOrder.get(1)));
+        buyOrders.add(buyOrder);
+      }
+
+      final List<MarketOrder> sellOrders = new ArrayList<>();
+      for (KrakenMarketOrder krakenSellOrder : krakenOrderBook.asks) {
+        final MarketOrder sellOrder =
+            new MarketOrderImpl(
+                OrderType.SELL,
+                krakenSellOrder.get(0),
+                krakenSellOrder.get(1),
+                krakenSellOrder.get(0).multiply(krakenSellOrder.get(1)));
+        sellOrders.add(sellOrder);
+      }
+      return new MarketOrderBookImpl(marketId, sellOrders, buyOrders);
+    } else {
+      final String errorMsg = FAILED_TO_GET_MARKET_ORDERS + krakenResponse;
+      LOG.error(errorMsg);
+      throw new TradingApiException(errorMsg);
+    }
+  }
+
+  private boolean adaptKrakenCancelOrderResult(KrakenResponse krakenResponse) {
+    // Assume we'll always get something here if errors array is empty; else blow fast wih NPE
+    final KrakenCancelOrderResult krakenCancelOrderResult =
+        (KrakenCancelOrderResult) krakenResponse.result;
+    if (krakenCancelOrderResult != null) {
+      if (krakenCancelOrderResult.count > 0) {
+        return true;
+      } else {
+        final String errorMsg = FAILED_TO_CANCEL_ORDER + krakenResponse;
+        LOG.error(errorMsg);
+        return false;
+      }
+    } else {
+      final String errorMsg = FAILED_TO_CANCEL_ORDER + krakenResponse;
+      LOG.error(errorMsg);
+      return false;
+    }
+  }
+
+  private BalanceInfoImpl adaptKrakenBalanceInfo(ExchangeHttpResponse response, Type resultType)
+      throws ExchangeNetworkException, TradingApiException {
+    final KrakenResponse krakenResponse = gson.fromJson(response.getPayload(), resultType);
+    if (krakenResponse != null) {
+      final List errors = krakenResponse.error;
+      if (errors == null || errors.isEmpty()) {
+        // Assume we'll always get something here if errors array is empty; else blow fast wih NPE
+        final KrakenBalanceResult balanceResult = (KrakenBalanceResult) krakenResponse.result;
+        final Map<String, BigDecimal> balancesAvailable = new HashMap<>();
+        final Set<Map.Entry<String, BigDecimal>> entries = balanceResult.entrySet();
+        for (final Map.Entry<String, BigDecimal> entry : entries) {
+          balancesAvailable.put(entry.getKey(), entry.getValue());
+        }
+
+        // 2nd arg of BalanceInfo constructor for reserved/on-hold balances is not provided by
+        // exchange.
+        return new BalanceInfoImpl(balancesAvailable, new HashMap<>());
+
+      } else {
+        if (isExchangeUndergoingMaintenance(response) && keepAliveDuringMaintenance) {
+          LOG.warn(() -> UNDER_MAINTENANCE_WARNING_MESSAGE);
+          throw new ExchangeNetworkException(UNDER_MAINTENANCE_WARNING_MESSAGE);
+        }
+
+        final String errorMsg = FAILED_TO_GET_BALANCE + response;
+        LOG.error(errorMsg);
+        throw new TradingApiException(errorMsg);
+      }
+    } else {
+      final String errorMsg = FAILED_TO_GET_BALANCE + response;
+      LOG.error(errorMsg);
+      throw new TradingApiException(errorMsg);
+    }
+  }
+
+  private void initGson() {
+    final GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.registerTypeAdapter(KrakenTickerResult.class, new KrakenTickerResultDeserializer());
+    gson = gsonBuilder.create();
+  }
+
+  private static boolean isExchangeUndergoingMaintenance(ExchangeHttpResponse response) {
+    if (response != null) {
+      final String payload = response.getPayload();
+      return payload != null && payload.contains(EXCHANGE_UNDERGOING_MAINTENANCE_RESPONSE);
+    }
+    return false;
+  }
+
+  /*
+   * Hack for unit-testing map params passed to transport layer.
+   */
+  private Map<String, String> createRequestParamMap() {
+    return new HashMap<>();
+  }
+
+  /*
+   * Hack for unit-testing header params passed to transport layer.
+   */
+  private Map<String, String> createHeaderParamMap() {
+    return new HashMap<>();
+  }
+
+  /*
+   * Hack for unit-testing transport layer.
+   */
+  private ExchangeHttpResponse makeNetworkRequest(
+      URL url, String httpMethod, String postData, Map<String, String> requestHeaders)
+      throws TradingApiException, ExchangeNetworkException {
+    return super.sendNetworkRequest(url, httpMethod, postData, requestHeaders);
+  }
+}
