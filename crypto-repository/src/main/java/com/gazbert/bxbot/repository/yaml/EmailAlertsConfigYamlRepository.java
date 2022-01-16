f20576891ff7e433dd934e0345ae7aa0e548de5e
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-01-16 15:47:26 +0000

    default update

--- crypto-repository/src/main/java/com/gazbert/bxbot/repository/yaml/EmailAlertsConfigYamlRepository.java
+++ crypto-repository/src/main/java/com/gazbert/bxbot/repository/yaml/EmailAlertsConfigYamlRepository.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Gareth Jon Lynch
+ * Copyright (c) 2019 gazbert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -16,28 +16,4 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-package com.gazbert.crypto.repository;
-
-import com.gazbert.crypto.domain.strategy.StrategyConfig;
-import java.util.List;
-
-/**
- * The Strategy configuration repository.
- *
- * @author gazbert
- */
-public interface StrategyConfigRepository {
-
-  List<StrategyConfig> findAll();
-
-  StrategyConfig findById(String id);
-
-  StrategyConfig save(StrategyConfig config);
-
-  StrategyConfig delete(String id);
-}
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
