889b1b47c1ba4ff2d4fdb3a9b6c9d9f1d6969d8f
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-02-19 16:25:06 +0000

    hoho

--- crypto-repository/src/test/java/com/gazbert/bxbot/repository/yaml/TestEngineConfigYamlRepository.java
+++ crypto-repository/src/test/java/com/gazbert/bxbot/repository/yaml/TestEngineConfigYamlRepository.java
@@ -13,4 +13,29 @@
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
- * TH
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.repository.yaml;
+
+import static com.gazbert.crypto.datastore.yaml.FileLocations.ENGINE_CONFIG_YAML_FILENAME;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+
+import com.gazbert.crypto.datastore.yaml.ConfigurationManager;
+import com.gazbert.crypto.datastore.yaml.engine.EngineType;
+import com.gazbert.crypto.domain.engine.EngineConfig;
+import com.gazbert.crypto.repository.EngineConfigRepository;
+import java.math.BigDecimal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.Powe
