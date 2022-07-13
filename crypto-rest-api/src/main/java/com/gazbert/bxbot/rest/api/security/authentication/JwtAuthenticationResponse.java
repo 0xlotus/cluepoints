16ed6f39948435a625ea1938ac546744249696ff
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-07-13 22:53:44 +0000

    Regenerate gemspec for version 0.2.4

--- crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/authentication/JwtAuthenticationResponse.java
+++ crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/authentication/JwtAuthenticationResponse.java
@@ -24,4 +24,32 @@
 
 package com.gazbert.crypto.rest.api.security.authentication;
 
-import javax.validation.constra
+import javax.validation.constraints.NotNull;
+
+/**
+ * Encapsulates a JWT Authentication response. It wraps the JWT (Base64 encoded String).
+ *
+ * @author gazbert
+ */
+public class JwtAuthenticationResponse {
+
+  @NotNull(message = "Token cannot be null")
+  private String token;
+
+  // For Jackson
+  public JwtAuthenticationResponse() {
+    token = "";
+  }
+
+  public JwtAuthenticationResponse(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return this.token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+}
