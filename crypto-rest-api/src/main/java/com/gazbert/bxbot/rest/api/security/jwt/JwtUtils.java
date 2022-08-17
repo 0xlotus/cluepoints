5a03a1b82ed788c7cd1c53cd6dbe5376c0ea4918
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-08-17 19:07:07 +0000

    structural changes

--- crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/jwt/JwtUtils.java
+++ crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/jwt/JwtUtils.java
@@ -17,4 +17,35 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABI
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.rest.api.security.jwt;
+
+import com.gazbert.crypto.rest.api.security.authentication.JwtAuthenticationException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+/**
+ * Util class for validating and accessing JSON Web Tokens.
+ *
+ * <p>Properties are loaded from the config/application.properties file.
+ *
+ * <p>Code originated from the excellent JWT and Spring Boot example by Stephan Zerhusen
