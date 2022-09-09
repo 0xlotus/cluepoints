040fef87b11140944f2345fea6c15a1fe80ac87c
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-09-09 16:58:21 +0000

    version for master branch release

--- crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/model/Role.java
+++ crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/model/Role.java
@@ -59,4 +59,29 @@ public class Role {
   private RoleName name;
 
   @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-  private L
+  private List<User> users;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public RoleName getName() {
+    return name;
+  }
+
+  public void setName(RoleName name) {
+    this.name = name;
+  }
+
+  public List<User> getUsers() {
+    return users;
+  }
+
+  public void setUsers(List<User> users) {
+    this.users = users;
+  }
+}
