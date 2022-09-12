3e549beae6eb87193caf09179fd4e2cfa7513daf
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-09-12 14:11:15 +0000

    ''

--- crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/v1/config/EngineConfigController.java
+++ crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/v1/config/EngineConfigController.java
@@ -15,4 +15,27 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PAR
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.rest.api.v1.config;
+
+import static com.gazbert.crypto.rest.api.v1.EndpointLocations.CONFIG_ENDPOINT_BASE_URI;
+
+import com.gazbert.crypto.domain.engine.EngineConfig;
+import com.gazbert.crypto.services.config.EngineConfigService;
+import io.swagger.annotations.Api;
+import java.security.Principal;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMap
