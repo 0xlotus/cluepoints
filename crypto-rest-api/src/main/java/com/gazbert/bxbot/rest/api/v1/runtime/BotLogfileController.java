2536299fe159e904ead862ef4be5d54c12cb6371
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-09-29 10:18:28 +0000

    Added Travis CI configuration

--- crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/v1/runtime/BotLogfileController.java
+++ crypto-rest-api/src/main/java/com/gazbert/bxbot/rest/api/v1/runtime/BotLogfileController.java
@@ -17,4 +17,31 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF C
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.rest.api.v1.runtime;
+
+import static com.gazbert.crypto.rest.api.v1.EndpointLocations.RUNTIME_ENDPOINT_BASE_URI;
+
+import com.gazbert.crypto.rest.api.RestApiConfig;
+import com.gazbert.crypto.services.runtime.BotLogfileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import java.io.IOException;
+import java.security.Principal;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframew
