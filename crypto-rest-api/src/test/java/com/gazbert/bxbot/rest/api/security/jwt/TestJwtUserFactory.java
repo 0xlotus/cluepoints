5fa2f4cb36e7f9a2339c0fbbafa20f9d08859274
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-10-23 17:42:53 +0000

    newStuff

--- crypto-rest-api/src/test/java/com/gazbert/bxbot/rest/api/security/jwt/TestJwtUserFactory.java
+++ crypto-rest-api/src/test/java/com/gazbert/bxbot/rest/api/security/jwt/TestJwtUserFactory.java
@@ -8,4 +8,33 @@
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject t
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.rest.api.security.jwt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.gazbert.crypto.rest.api.security.model.Role;
+import com.gazbert.crypto.rest.api.security.model.RoleName;
+import com.gazbert.crypto.rest.api.security.model.User;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+/**
+ * Te
