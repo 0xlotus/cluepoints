1e56fec78fe4d7edc92f36332ef8e3214e7bb2f6
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-11-25 16:12:19 +0000

    More code refactoring

--- crypto-rest-api/src/test/java/com/gazbert/bxbot/rest/api/v1/runtime/TestBotRestartController.java
+++ crypto-rest-api/src/test/java/com/gazbert/bxbot/rest/api/v1/runtime/TestBotRestartController.java
@@ -15,4 +15,26 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.rest.api.v1.runtime;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.gazbert.crypto.core.engine.TradingEngine;
+import com.gazbert.crypto.core.mail.EmailAlerter;
+import com.gazbert.crypto.services.runtime.BotRestartService;
+import org.junit.Before;
+import org.junit.Test;
+impo
