b6dea742062729761b49d4dac16b42e7357a5c8a
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-12-10 20:25:24 +0000

    fixed quotes

--- crypto-services/src/main/java/com/gazbert/bxbot/services/config/EngineConfigService.java
+++ crypto-services/src/main/java/com/gazbert/bxbot/services/config/EngineConfigService.java
@@ -16,4 +16,23 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COP
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.services.config;
+
+import com.gazbert.crypto.domain.engine.EngineConfig;
+
+/**
+ * The Engine configuration service.
+ *
+ * @author gazbert
+ */
+public interface EngineConfigService {
+
+  EngineConfig getEngineConfig();
+
+  EngineConfig updateEngineConfig(EngineConfig config);
+}
