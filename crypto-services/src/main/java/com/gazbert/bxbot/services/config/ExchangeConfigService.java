ad7ad11a04a0f34f7e03ec5289a62e6ec0e81df4
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-12-11 09:32:28 +0000

    update docblock

--- crypto-services/src/main/java/com/gazbert/bxbot/services/config/ExchangeConfigService.java
+++ crypto-services/src/main/java/com/gazbert/bxbot/services/config/ExchangeConfigService.java
@@ -13,4 +13,26 @@
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
- * THE SOFTWAR
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.services.config;
+
+import com.gazbert.crypto.domain.exchange.ExchangeConfig;
+
+/**
+ * The Exchange configuration service.
+ *
+ * @author gazbert
+ */
+public interface ExchangeConfigService {
+
+  ExchangeConfig getExchangeConfig();
+
+  ExchangeConfig updateExchangeConfig(ExchangeConfig config);
+}
