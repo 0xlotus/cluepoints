d50dc572aa93c8a4e9a88a2042a3e7f3b983d857
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-12-24 10:51:53 +0000

    Minor clean

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
@@ -21,4 +21,35 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.gazbert.crypto
+package com.gazbert.crypto.services.runtime.impl;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.springframework.boot.actuate.logging.LogFileWebEndpoint;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Tests Bot logfile service behaves as expected.
+ *
+ * @author gazbert
+ */
+public class TestBotLogfileService {
+
+  @Test
+  public void whenGetLogfileCalledThenExpectLogfileContentToBeReturned() throws Exception {
+    final String logfilePath = "src/test/logfiles/logfile.log";
+    final String expectedLogfileContent =
+        "4981 [main] 2019-07-20 17:30:20,429 INFO  EngineConfigYamlRepository get() "
+            + "- Fetching EngineConfig..."
+            + System.lineSeparator()
+            + "4982 [main] 2019-07-20 17:30:21,429 INFO  EngineConfigYamlRepository get() "
+            + "- Val
