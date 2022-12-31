5d7791c92a85f5fe494eb1e17acd259c6dc410bb
0xlotus <89240464+0xlotus@users.noreply.github.com>
2022-12-31 18:44:13 +0000

    chore(deps): lock file maintenance

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
@@ -102,4 +102,30 @@ public class TestBotLogfileService {
     final String logfilePath = "src/test/logfiles/logfile.log";
     final String expectedLogfileContent =
         "4982 [main] 2019-07-20 17:30:21,429 INFO  EngineConfigYamlRepository get() "
-            + "- Validating config...
+            + "- Validating config..."
+            + System.lineSeparator()
+            + "4983 [main] 2019-07-20 17:30:22,429 INFO  EngineConfigYamlRepository get() "
+            + "- Config is good"
+            + System.lineSeparator();
+
+    final Path path = FileSystems.getDefault().getPath(logfilePath);
+    final Resource resource = new FileSystemResource(path);
+    final LogFileWebEndpoint logFileWebEndpoint = EasyMock.createMock(LogFileWebEndpoint.class);
+
+    expect(logFileWebEndpoint.logFile()).andReturn(resource);
+    replay(logFileWebEndpoint);
+
+    final BotLogfileServiceImpl botLogfileService = new BotLogfileServiceImpl(logFileWebEndpoint);
+    final String fetchedLogfile = botLogfileService.getLogfileTail(2); // tail 2 lines only
+
+    assertThat(fetchedLogfile).isEqualTo(expectedLogfileContent);
+    verify(logFileWebEndpoint);
+  }
+
+  @Test
+  public void whenLogfileTailCalledWith4ThenExpectOnly3LinesToBeReturned() throws Exception {
+    final String logfilePath = "src/test/logfiles/logfile.log";
+    final String expectedLogfileContent =
+        "4981 [main] 2019-07-20 17:30:20,429 INFO  EngineConfigYamlRepository get() "
+            + "- Fetching EngineConfig..."
+            + System.
