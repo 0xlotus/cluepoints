0b29fa602d85e3b8096ceac5269ab1ff89d639b4
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-01 12:28:15 +0000

    split

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
@@ -178,4 +178,30 @@ public class TestBotLogfileService {
   @Test
   public void whenLogfileHeadCalledWith4ThenExpectOnly3LinesToBeReturned() throws Exception {
     final String logfilePath = "src/test/logfiles/logfile.log";
-    final Strin
+    final String expectedLogfileContent =
+        "4981 [main] 2019-07-20 17:30:20,429 INFO  EngineConfigYamlRepository get() "
+            + "- Fetching EngineConfig..."
+            + System.lineSeparator()
+            + "4982 [main] 2019-07-20 17:30:21,429 INFO  EngineConfigYamlRepository get() "
+            + "- Validating config..."
+            + System.lineSeparator()
+            + "4983 [main] 2019-07-20 17:30:22,429 INFO  EngineConfigYamlRepository get() "
+            + "- Config is good"
+            + System.lineSeparator();
+
+    final Path path = FileSystems.getDefault().getPath(logfilePath);
+    final Resource resource = new FileSystemResource(path);
+    final LogFileWebEndpoint logFileWebEndpoint = EasyMock.createMock(LogFileWebEndpoint.class);
+
+    expect(logFileWebEndpoint.logFile()).andReturn(resource);
+    replay(logFileWebEndpoint);
+
+    final BotLogfileServiceImpl botLogfileService = new BotLogfileServiceImpl(logFileWebEndpoint);
+    final String fetchedLogfile = botLogfileService.getLogfileHead(4); // attempt 4 lines
+
+    assertThat(fetchedLogfile).isEqualTo(expectedLogfileContent); // expect first 3
+    verify(logFileWebEndpoint);
+  }
+
+  @Test
+  public void whenGetLogf
