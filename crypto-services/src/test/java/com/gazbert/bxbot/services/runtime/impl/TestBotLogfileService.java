67c5d8a69a34cdf5e5eaff30ed712c5a1444dbbc
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-01 09:18:01 +0000

    Rename component

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
@@ -152,4 +152,30 @@ public class TestBotLogfileService {
 
   @Test
   public void whenLogfileHeadCalledWith2ThenExpectOnlyLast2LinesToBeReturned() throws Exception {
-    final String logfilePath = "src/test/logfiles/logfile.
+    final String logfilePath = "src/test/logfiles/logfile.log";
+    final String expectedLogfileContent =
+        "4981 [main] 2019-07-20 17:30:20,429 INFO  EngineConfigYamlRepository get() "
+            + "- Fetching EngineConfig..."
+            + System.lineSeparator()
+            + "4982 [main] 2019-07-20 17:30:21,429 INFO  EngineConfigYamlRepository get() "
+            + "- Validating config..."
+            + System.lineSeparator();
+
+    final Path path = FileSystems.getDefault().getPath(logfilePath);
+    final Resource resource = new FileSystemResource(path);
+    final LogFileWebEndpoint logFileWebEndpoint = EasyMock.createMock(LogFileWebEndpoint.class);
+
+    expect(logFileWebEndpoint.logFile()).andReturn(resource);
+    replay(logFileWebEndpoint);
+
+    final BotLogfileServiceImpl botLogfileService = new BotLogfileServiceImpl(logFileWebEndpoint);
+    final String fetchedLogfile = botLogfileService.getLogfileHead(2); // head 2 lines only
+
+    assertThat(fetchedLogfile).isEqualTo(expectedLogfileContent);
+    verify(logFileWebEndpoint);
+  }
+
+  @Test
+  public void whenLogfileHeadCalledWith4ThenExpectOnly3LinesToBeReturned() throws Exception {
+    final String logfilePath = "src/test/logfiles/logfile.log";
+    final Strin
