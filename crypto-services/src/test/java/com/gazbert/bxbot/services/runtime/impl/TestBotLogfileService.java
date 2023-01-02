22acc6633b8ec2149f7b86838d9210ce424c2f46
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-02 08:22:24 +0000

    Pontoon: Update Albanian (sq) localization of AMO Frontend

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotLogfileService.java
@@ -204,4 +204,26 @@ public class TestBotLogfileService {
   }
 
   @Test
-  public void whenGetLogf
+  public void whenGetLogfileAsResourceCalledThenExpectLogfileToBeReturned() throws Exception {
+    final String logfilePath = "src/test/logfiles/logfile.log";
+    final String expectedLogfileContent =
+        "4981 [main] 2019-07-20 17:30:20,429 INFO  EngineConfigYamlRepository get() "
+            + "- Fetching EngineConfig..."
+            + System.lineSeparator()
+            + "4982 [main] 2019-07-20 17:30:21,429 INFO  EngineConfigYamlRepository get() "
+            + "- Validating config..."
+            + System.lineSeparator()
+            + "4983 [main] 2019-07-20 17:30:22,429 INFO  EngineConfigYamlRepository get() "
+            + "- Config is good";
+
+    final Path path = FileSystems.getDefault().getPath(logfilePath);
+    final Resource resource = new FileSystemResource(path);
+    final LogFileWebEndpoint logFileWebEndpoint = EasyMock.createMock(LogFileWebEndpoint.class);
+
+    expect(logFileWebEndpoint.logFile()).andReturn(resource);
+    replay(logFileWebEndpoint);
+
+    final BotLogfileServiceImpl botLogfileService = new BotLogfileServiceImpl(logFileWebEndpoint);
+    final int maxLogfileSizeInBytes = 1024;
+    final Resource logfileAsResource =
+        botLogfileService.getLogfileAsResource(maxLogfileSizeInBytes)
