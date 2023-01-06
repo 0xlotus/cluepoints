e6031d2f14627fb7906e67bee1acec5b2d3fb1f4
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-06 13:01:40 +0000

    build cleanup

--- crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotStatusService.java
+++ crypto-services/src/test/java/com/gazbert/bxbot/services/runtime/impl/TestBotStatusService.java
@@ -17,4 +17,39 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR I
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.services.runtime.impl;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.health.Status;
+
+/**
+ * Tests Bot status service behaves as expected.
+ *
+ * @author gazbert
+ */
+public class TestBotStatusService {
+
+  @Test
+  public void whenGetStatusCalledThenExpectBotStatusToBeReturned() {
+    final String botStatus = "UP";
+    final Status upStatus = new Status(botStatus);
+    final Health health = Health.status(upStatus).build();
+    final HealthEndpoint healthEndpoint = EasyMock.createMock(HealthEndpoint.class);
+
+    expect(healthEndpoint.health()).andReturn(health);
+    replay(healthEndpoint);
+
+    final BotStatusServiceImpl botStatusService = new BotStatusServiceImpl(healthEndpoint);
+    final String fetch
