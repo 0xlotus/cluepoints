f79f878b5108a1ecae08ff14630929b9dcefcf05
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-18 16:52:16 +0000

    [maven-release-plugin] prepare release v1.5

--- crypto-strategy-api/src/main/java/com/gazbert/bxbot/strategy/api/StrategyConfig.java
+++ crypto-strategy-api/src/main/java/com/gazbert/bxbot/strategy/api/StrategyConfig.java
@@ -17,4 +17,48 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, A
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.strategy.api;
+
+import java.util.Set;
+
+/**
+ * <p>
+ * Encapsulates any (optional) configuration for a Trading Strategy.
+ * Basically just a map of key-value pairs.
+ * </p>
+ *
+ * <p>
+ * Configuration comes from the strategies.yaml file.
+ * </p>
+ *
+ * @author gazbert
+ * @since 1.0
+ */
+public interface StrategyConfig {
+
+  /**
+   * Fetches a config item for a given key.
+   *
+   * @param key the key of the item to fetch.
+   * @return value of the item if found, null otherwise.
+   */
+  String getConfigItem(String key);
+
+  /**
+   * Returns the number of config items.
+   *
+   * @return the number of config items.
+   */
+  int getNumberOfConfigItems();
+
+  /**
+   * Returns all of the config item keys.
+   *
+   * @return all of the config item keys.
+   */
+  Set<String> getConfigItemKeys();
+}
