eb23f9f819cdb4bbf2227967e2a2d5015bf51e6e
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-01-30 08:42:41 +0000

    fix missing files

--- crypto-strategy-api/src/main/java/com/gazbert/bxbot/strategy/api/TradingStrategy.java
+++ crypto-strategy-api/src/main/java/com/gazbert/bxbot/strategy/api/TradingStrategy.java
@@ -8,4 +8,37 @@
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following condition
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.strategy.api;
+
+import com.gazbert.crypto.trading.api.Market;
+import com.gazbert.crypto.trading.api.TradingApi;
+
+/**
+ * All user defined Trading Strategies must implement this interface.
+ *
+ * <p>The Trading Engine will send only 1 thread through your strategy code at a time - you do not
+ * have to code for concurrency.
+ *
+ * @author gazbert
+ * @since 1.0
+ */
+public interface TradingStrategy {
+
+  /**
+   * Called once by the Trading Engine when it starts up.
+   *
+   * @param tradingApi the Trading API.
+   * @
