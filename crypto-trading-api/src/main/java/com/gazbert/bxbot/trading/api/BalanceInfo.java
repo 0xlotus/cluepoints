23adbe0ebc5eaa7c59f9d9149c51efd0e3daf766
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-02-04 12:12:16 +0000

    Update transcoding.html

--- crypto-trading-api/src/main/java/com/gazbert/bxbot/trading/api/BalanceInfo.java
+++ crypto-trading-api/src/main/java/com/gazbert/bxbot/trading/api/BalanceInfo.java
@@ -10,4 +10,42 @@
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
  *
- * The above copyright notice and this permission
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.trading.api;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * Encapsulates wallet balance info held on the exchange.
+ *
+ * @author gazbert
+ * @since 1.0
+ */
+public interface BalanceInfo {
+
+  /**
+   * Returns map of available balances.
+   *
+   * <p>The key is the currency id in UPPERCASE, e.g. LTC, BTC, USD
+   *
+   * @return map of available balances.
+   */
+  Map<String, BigDecimal> getBalancesAvailable();
+
+  /**
+   * Returns map of balances on hold.
+   *
+   * <p>Some exchanges do not provide this information and the returned map will be empty.
+   *
+   * <p>The key is t
