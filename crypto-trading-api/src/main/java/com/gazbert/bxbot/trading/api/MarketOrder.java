a7c6032607a5080b7ab168eedbbec9e5ca993487
0xlotus <89240464+0xlotus@users.noreply.github.com>
2023-02-16 05:32:30 +0000

    Fix code comment

--- crypto-trading-api/src/main/java/com/gazbert/bxbot/trading/api/MarketOrder.java
+++ crypto-trading-api/src/main/java/com/gazbert/bxbot/trading/api/MarketOrder.java
@@ -17,4 +17,50 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, AR
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.crypto.trading.api;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents a Market Order on the exchange.
+ *
+ * <p>The type of order (buy/sell) is determined by the {@link OrderType}.
+ *
+ * @author gazbert
+ * @since 1.0
+ */
+public interface MarketOrder {
+
+  /**
+   * Returns the type of order. Value will be {@link OrderType#BUY} or {@link OrderType#SELL}.
+   *
+   * @return the type of order.
+   */
+  OrderType getType();
+
+  /**
+   * Returns the price of the order. This is usually in BTC or USD.
+   *
+   * @return Price of the order.
+   */
+  BigDecimal getPrice();
+
+  /**
+   * Returns the quantity of the order. This is usually the amount of the other currency you want to
+   * trade for BTC/USD.
+   *
+   * @return Quantity of the order.
+   */
+  BigDecimal getQuantity();
+
+  /**
+   * Returns the total value of order (price * quantity). This is usually in BTC or USD.
+   *
+   * @return Total value of order (price * quantity).
+   */
+  BigDecimal getTotal();
+}
